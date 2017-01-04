40eb95d0ab5a9dd5cca4c7b36e70a811e7f7362d
get <waldemar.reusch@googlemail.com>
2017-01-05 00:00:30 +0100

    improve docs

--- src/main/file-picker.js
+++ src/main/file-picker.js
@@ -1,3 +1,14 @@
+/**
+ * Force-open a filePicker dialog, like the one seen when
+ * clicking on a file selection input. Will call {@code callback}
+ * with the `FileList` object, containing the selected files.
+ *
+ * @param {{accept: String, multiple: boolean, dir: boolean}} opts an options object
+ * @param opts.accept - a comma-delimited string of accepted mime-types
+ * @param opts.multiple - whether to accept multiple files, defaults to false
+ * @param opts.dir - whether to accept directory selection, defaults to false
+ * @param {function} callback
+ */
 module.exports = function filePicker (opts, callback) {
   if (arguments.length === 1) {
     callback = opts
