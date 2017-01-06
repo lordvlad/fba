65fa706e9512dddcbcd3cb893c1bb6312ea45e41
get <waldemar.reusch@googlemail.com>
2017-01-06 16:11:44 +0100

    add progress bar for service operations

--- src/main/services/index.js
+++ src/main/services/index.js
@@ -1,5 +1,6 @@
 /* global URL */
 const work = require('webworkify')
+const np = require('nprogress')
 
 const { defer } = require('../util')
 const fba = work(require('./fba.js'))
@@ -8,15 +9,31 @@ module.exports.search = require('./search')
 module.exports.parse = require('./parse')
 
 module.exports.fba = function (opt) {
+  np.start()
   const { promise, resolve, reject } = defer()
   const clean = () => {
     fba.removeEventListener('message', onMessage)
     fba.removeEventListener('message', onError)
     URL.revokeObjectURL(fba.objectURL)
+    np.done()
   }
 
-  function onMessage (e) { if (e.data.debug) return console.log(e.data.debug); clean(); resolve(e.data) }
-  function onError (e) { console.log(e.data); clean(); reject(e) }
+  function onMessage (e) {
+    if (e.data.debug) {
+      np.inc()
+      console.log(e.data.debug)
+    } else if (e.data.progress) {
+      np.set(e.data.progress)
+    } else {
+      clean()
+      resolve(e.data)
+    }
+  }
+  function onError (e) {
+    console.log(e.data)
+    clean()
+    reject(e)
+  }
 
   fba.addEventListener('message', onMessage)
   fba.addEventListener('error', onError)
