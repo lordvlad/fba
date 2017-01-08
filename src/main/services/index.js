af2fd8799d32fa59784a28c759201dd0fee6c9fc
get <waldemar.reusch@googlemail.com>
2017-01-08 18:17:32 +0100

    refactor services

--- src/main/services/index.js
+++ src/main/services/index.js
@@ -3,42 +3,53 @@ const work = require('webworkify')
 const np = require('nprogress')
 
 const { defer } = require('../util')
-const fba = work(require('./fba.js'))
+
+const services = {
+  fba: require('./fba.js'),
+  biomodels: require('./biomodels.js')
+}
+const workers = {}
 
 module.exports.search = require('./search')
 module.exports.parse = require('./parse')
+module.exports.fba = service('fba')
+module.exports.biomodels = window.biomodels = service('biomodels')
 
-module.exports.fba = function (opt) {
-  np.start()
-  const { promise, resolve, reject } = defer()
-  const clean = () => {
-    fba.removeEventListener('message', onMessage)
-    fba.removeEventListener('message', onError)
-    URL.revokeObjectURL(fba.objectURL)
-    np.done()
-  }
-
-  function onMessage (e) {
-    if (e.data.debug) {
-      np.inc()
-      console.log(e.data.debug)
-    } else if (e.data.progress) {
-      np.set(e.data.progress)
-    } else {
-      clean()
-      resolve(e.data)
+function service (name) {
+  return function (opt) {
+    np.start()
+    if (!workers[name]) workers[name] = work(services[name])
+    const srv = workers[name]
+    const { promise, resolve, reject } = defer()
+    const clean = () => {
+      srv.removeEventListener('message', onMessage)
+      srv.removeEventListener('message', onError)
+      URL.revokeObjectURL(srv.objectURL)
+      np.done()
     }
+
+    function onMessage (e) {
+      if (e.data.debug) {
+        np.inc()
+        console.log(e.data.debug)
+      } else if (e.data.progress) {
+        np.set(e.data.progress)
+      } else {
+        clean()
+        resolve(e.data)
+      }
+    }
+    function onError (e) {
+      console.log(e.data)
+      clean()
+      reject(e)
+    }
+
+    srv.addEventListener('message', onMessage)
+    srv.addEventListener('error', onError)
+
+    srv.postMessage(opt)
+
+    return promise
   }
-  function onError (e) {
-    console.log(e.data)
-    clean()
-    reject(e)
-  }
-
-  fba.addEventListener('message', onMessage)
-  fba.addEventListener('error', onError)
-
-  fba.postMessage(opt)
-
-  return promise
 }
