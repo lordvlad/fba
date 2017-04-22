b9f9ad585d9f8f8c72bbc3dfe964a23aefbc8407
get <waldemar.reusch@googlemail.com>
2017-04-23 00:56:16 +0200

    fix style

--- src/main/proxy.js
+++ src/main/proxy.js
@@ -3,9 +3,7 @@ const port = process.env.PORT || 8080
 
 const corsProxy = require('cors-anywhere')
 corsProxy.createServer({
-  originWhitelist: [], // Allow all origins 
-  // requireHeader: ['origin', 'x-requested-with'],
-  // removeHeaders: ['cookie', 'cookie2']
+  originWhitelist: [] // Allow all origins
 }).listen(port, host, function () {
   console.log('Running CORS Anywhere on ' + host + ':' + port)
 })
