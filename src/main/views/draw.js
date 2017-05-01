aa5ef6d2441b2b6fd6eb6105c80bcea9785fd2f0
get <waldemar.reusch@googlemail.com>
2017-05-01 20:11:46 +0200

    use const and clean shadowroot

--- src/main/views/draw.js
+++ src/main/views/draw.js
@@ -6,7 +6,7 @@ const { get } = require('../util')
 
 const either = (cond, defaul, ...arr) => (d) => {
   for (let a of arr) {
-    let v = a.call ? a(d) : a
+    const v = a.call ? a(d) : a
     if (cond(v)) return v
   }
   return defaul
@@ -17,12 +17,12 @@ const getle0 = (...args) => either(le0, 0, get(...args))
 const margin = 10
 
 function makeEdgeBetween (source, target, ah) {
-  let si = source.rayIntersection(target.cx(), target.cy()) || { x: source.cx(), y: source.cy() }
-  let ti = target.rayIntersection(source.cx(), source.cy()) || { x: target.cx(), y: target.cy() }
-  let dx = ti.x - si.x
-  let dy = ti.y - si.y
-  let l = Math.sqrt(dx * dx + dy * dy)
-  let al = l - ah
+  const si = source.rayIntersection(target.cx(), target.cy()) || { x: source.cx(), y: source.cy() }
+  const ti = target.rayIntersection(source.cx(), source.cy()) || { x: target.cx(), y: target.cy() }
+  const dx = ti.x - si.x
+  const dy = ti.y - si.y
+  const l = Math.sqrt(dx * dx + dy * dy)
+  const al = l - ah
   return {
     sourceIntersection: si,
     targetIntersection: ti,
@@ -38,17 +38,17 @@ const makeRoute = (d) => {
 const translateLabel = (d) => `translate(${d.innerBounds.cx()},${d.y + margin - d.height / 2})`
 
 function setSize (d) {
-  let b = this.getBBox()
+  const b = this.getBBox()
   assign(d, {width: b.width + 2 * margin + 8, height: b.height + 2 * margin + 8})
   d.innerBounds = d.bounds.inflate(-margin)
 }
 
 function insertLineBreaks (d) {
-  var el = d3.select(this)
-  var words = d.label.split(' ')
+  const el = d3.select(this)
+  const words = d.label.split(' ')
   el.text('')
 
-  for (var i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     el.append('tspan')
       .text(words[i])
       .attr('x', 0)
@@ -58,16 +58,19 @@ function insertLineBreaks (d) {
 };
 
 module.exports = function draw (model, root) {
-  let { graph } = model
-  let width = root.offsetWidth - 10
-  let height = root.offsetHeight - 10
-  let d3cola = cola.d3adaptor()
+  const { graph } = model
+  const width = root.offsetWidth - 10
+  const height = root.offsetHeight - 10
+  const shadow = root.shadowRoot || root.createShadowRoot()
+  const d3cola = cola.d3adaptor()
     .linkDistance(40)
     .avoidOverlaps(true)
     .size([width, height])
 
-  let outer = d3
-    .select(root.createShadowRoot())
+  while (shadow.firstChild) shadow.firstChild.remove()
+
+  const outer = d3
+    .select(shadow)
     .append('svg')
     .attr('width', width)
     .attr('height', height)
@@ -83,14 +86,14 @@ module.exports = function draw (model, root) {
       .scaleExtent([0.1, 10])
       .on('zoom', () => vis.attr('transform', `translate(${d3.event.translate}) scale(${d3.event.scale})`)))
 
-  let vis = outer.append('g')
+  const vis = outer.append('g')
 
-  let groupsLayer = vis.append('g')
-  let nodesLayer = vis.append('g')
-  let linksLayer = vis.append('g')
+  const groupsLayer = vis.append('g')
+  const nodesLayer = vis.append('g')
+  const linksLayer = vis.append('g')
 
   // define arrow markers for graph links
-  let defs = outer.append('svg:defs')
+  const defs = outer.append('svg:defs')
 
   defs.append('svg:marker')
     .attr('id', 'end-arrow')
@@ -124,14 +127,14 @@ module.exports = function draw (model, root) {
     // .start(20, 10, 10)
     .start(20)
 
-  let link = linksLayer
+  const link = linksLayer
     .selectAll('.link')
     .data(graph.links)
     .enter()
     .append('line')
     .attr('class', get('className'))
 
-  let group = groupsLayer
+  const group = groupsLayer
     .selectAll('.group')
     .data(graph.groups)
     .enter()
@@ -141,7 +144,7 @@ module.exports = function draw (model, root) {
     .attr('ry', get('ry'))
     // .call(d3cola.drag)
 
-  let node = nodesLayer
+  const node = nodesLayer
     .selectAll('.node')
     .data(graph.nodes)
     .enter()
@@ -151,7 +154,7 @@ module.exports = function draw (model, root) {
 
   node.append('title').text(get('label'))
 
-  let label = nodesLayer
+  const label = nodesLayer
     .selectAll('.label')
     .data(graph.nodes)
     .enter()
