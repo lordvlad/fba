84d59534a9fc91f9648a3b1bc290ed857db8c79e
get <waldemar.reusch@googlemail.com>
2017-05-27 21:24:20 +0200

    move to tachyons

--- src/main/views/file.js
+++ src/main/views/file.js
@@ -1,15 +1,22 @@
 const html = require('choo/html')
 const { li } = require('./util')
+const css = require('sheetify')
+
+const tdn = css`:host a {text-decoration: none;}`
 
 module.exports = function fileView (state, send) {
+  const lii = (title, icon, onclick) => li({title,
+    icon,
+    onclick,
+    styles: `pa3 ${tdn}`
+  })
+
   return html`
-    <div>
-      <nav class="vclNavigation vclLayoutVertical vclLayoutFlex vclVertical">
-        <ul>
-          ${li('new model', 'file-o', () => send('newModel'))}
-          ${li('open sbml', 'folder-open-o', () => send('openModelFile'))}
-        </ul>
-      </nav>
-    </div>
+    <ul class="list pa0 ma0">
+      ${lii('new model', 'file-o', () => send('newModel'))}
+      ${lii('open sbml', 'folder-open-o', () => send('openModelFile'))}
+    </ul>
   `
 }
+
+// ${search(state.search, send)}
--- src/main/views/index.js
+++ src/main/views/index.js
@@ -1,11 +1,24 @@
 const html = require('choo/html')
 const menuView = require('./menu')
 const contentView = require('./content')
+const css = require('sheetify')
+
+css`
+  html { height:100%; font-size: 14px; }
+  a { color: inherit;  }
+  .disabled a { pointer-events: none }
+ `
+
+css('tachyons')
+css('tachyons-debug-grid')
+css('font-awesome')
+css('nprogress')
+css('../styles/fontawesome-embedded-fonts.css')
 
 module.exports = function mainView (state, emit) {
   return html`
-    <body>
-      <div style="height:100%" class="vclContentArea vclLayoutHorizontal vclLayoutFlex">
+    <body class=h-100>
+      <div class="flex flex-row h-100">
         ${menuView(state, emit)}
         ${contentView(state, emit)}
       </div>
