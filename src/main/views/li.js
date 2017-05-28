1944d18c479552d730dd7d9ae61043df777dfbec
get <waldemar.reusch@googlemail.com>
2017-05-28 19:37:40 +0200

    enable children passthrough

--- src/main/views/li.js
+++ src/main/views/li.js
@@ -12,7 +12,7 @@ const constStyle = css`
 
 const notImpl = () => console.log('not implemented')
 const noop = () => {}
-const li = ({title = 'blub', icon = 'angle-right', onclick = notImpl, selected = false, disabled = false, text = true, styles = ''}) => html`
+const li = ({title = 'blub', icon = 'angle-right', onclick = notImpl, selected = false, disabled = false, text = true, styles = '', children}) => html`
   <li class="pa3 ${styles}
         ${disabled ? 'white-60 disabled' : `hover-black hover-bg-white-80 white ${constStyle}`}
         ${selected ? 'bg-white-80 black-20' : ''}"
@@ -24,7 +24,7 @@ const li = ({title = 'blub', icon = 'angle-right', onclick = notImpl, selected =
         tabindex=${disabled ? -1 : 0}
         class="link"
         onclick=${disabled ? noop : (e) => { killEvent(e); onclick(e) }}>
-      <i class="fa fa-fw fa-${icon}"></i>${text ? `\u00a0${title}` : ''}
+      <i class="fa fa-fw fa-${icon}"></i>${children || (text ? `\u00a0${title}` : '')}
     </a>
   </li>
 `
