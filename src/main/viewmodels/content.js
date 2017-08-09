6f16921a0e26b925f6dc2602554ced3ea3cfd6e4
get <waldemar.reusch@googlemail.com>
2017-08-09 20:40:50 +0200

    add example url for a sbmlfile with layout

--- src/main/viewmodels/content.js
+++ src/main/viewmodels/content.js
@@ -1,9 +1,12 @@
+const cors = require('../lib/cors-anywhere')
 const exampleId = 'BIOMD0000000172'
+const exampleUrl = cors + '/http://otto.bioquant.uni-heidelberg.de/sbml/level2/20080130/example5.xml'
 
 module.exports = function () {
   return function (state) {
     state.content = {
-      exampleId
+      exampleId,
+      exampleUrl
     }
   }
 }
