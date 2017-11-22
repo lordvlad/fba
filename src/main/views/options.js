067150724c3050acf7cb66ccb80e7dcf38b92fce
Waldemar Reusch <waldemar.reusch@googlemail.com>
2017-11-22 22:59:06 +0100

    add link to github

--- src/main/views/options.js
+++ src/main/views/options.js
@@ -1,10 +1,13 @@
 const html = require('choo/html')
 const li = require('./li')
 
+const homepage = 'https://github.com/lordvlad/fba'
+
 module.exports = function optionsView (state, send) {
+  const about = () => window.open(homepage)
   return html`
     <ul class="list pa0 ma0">
-      ${li('todo', 'question')}
+      ${li({title: 'about', icon: 'question', onclick: about})}
     </ul>
     `
 }
