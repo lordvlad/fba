f080975ff3edc7736e1251d172158756b9e2453f
Waldemar Reusch <waldemar.reusch@googlemail.com>
2017-11-22 23:05:35 +0100

    hide the menu quickly when workspace is tapped

--- src/main/views/content.js
+++ src/main/views/content.js
@@ -2,9 +2,10 @@ const html = require('choo/html')
 const modelView = require('./model')
 
 module.exports = function (state, emit) {
-  return html`
-    <div class="overflow-hidden w-100 v-top dib h-100">
-      ${modelView(state, emit)}
-    </div>
-  `
+  const v = modelView(state, emit)
+  if (state.menu.active) {
+    const closeMenu = () => emit('menu:active', state.menu.active)
+    return html`<div class="overflow-hidden w-100 v-top dib h-100" onmousedown=${closeMenu}>${v}</div>`
+  }
+  return html`<div class="overflow-hidden w-100 v-top dib h-100">${v}</div>`
 }
