19fe8f9c9dacdc1b3706f390ef930cab8337bc5e
Waldemar Reusch <waldemar.reusch@googlemail.com>
2017-11-26 18:34:25 +0100

    prepare model directory

--- src/main/views/network.js
+++ src/main/views/network.js
@@ -2,20 +2,24 @@ const html = require('choo/html')
 const li = require('./li')
 
 module.exports = function networkView (state, send) {
+  const wrap = (lis) => html`<ul class="list pa0 ma0">${lis}</ul>`
   const { directories, detail } = state.menu.network
   if (detail) {
-    return html`
-    <ul class="list pa0 ma0">
-      ${li({
+    return wrap(
+      li({
         title: detail.id,
         icon: 'info',
         text: detail.id,
         styles: 'underline'
-      })}
-    </ul>
-  `
+      }))
   }
   if (directories) {
+    return wrap(
+      li({title: 'TODO', text: 'TODO'})
+    )
     // TODO
   }
+  return wrap(
+    li({title: 'TODO', text: 'TODO'})
+  )
 }
