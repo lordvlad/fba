47eca5a84868d776c86bed507cd9a14088c8d88a
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-02-11 22:47:08 +0100

    improve network view

--- src/main/views/network.js
+++ src/main/views/network.js
@@ -1,18 +1,35 @@
 const html = require('choo/html')
 const li = require('./li')
 
+function wrap (lis) { return html`<ul class="list pa0 ma0">${lis}</ul>` }
+
+function detailView (detail) {
+  // for now its the same
+  return wrap(listSelectionElementView(detail))
+}
+
+function listSelectionView (selection) {
+  return wrap(selection.map(listSelectionElementView))
+}
+
+/**
+ *
+ * @param {*SNode} element
+ */
+function listSelectionElementView (element) {
+  const { id } = element
+  return li({
+    title: id,
+    icon: 'info',
+    text: id,
+    styles: 'underline'
+  })
+}
+
 module.exports = function networkView (state, send) {
-  const wrap = (lis) => html`<ul class="list pa0 ma0">${lis}</ul>`
   const { directories, detail } = state.menu.network
-  if (detail) {
-    return wrap(
-      li({
-        title: detail.id,
-        icon: 'info',
-        text: detail.id,
-        styles: 'underline'
-      }))
-  }
+  if (detail && detail.length) return listSelectionView(detail)
+  if (detail) return detailView(detail)
   if (directories) {
     return wrap(
       li({title: 'TODO', text: 'TODO'})
